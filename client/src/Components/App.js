332773986e4126a3aa5cf4eee675241edb7cb819
Hesh G <heshan.ua@gmail.com>
2021-01-26 18:11:53 +0530

    Added client login

--- client/src/Components/App.js
+++ client/src/Components/App.js
@@ -7,7 +7,7 @@ import 'semantic-ui-css/semantic.min.css';
 import MenuBar from './MenuBar';
 // import LandingPage from '../views/LandingPage';
 // import Game from '../views/Game';
-// import Login from '../views/Login';
+import Login from '../views/Login';
 import Register from '../views/Register';
 
 function App() {
@@ -15,9 +15,9 @@ function App() {
     <Router>
       <Container>
         <MenuBar />
-        {/* <Route exact path='/' component={Home} />
-        <Route exact path='/game' component={Game} />
-        <Route exact path='/login' component={Login} /> */}
+        {/* <Route exact path='/' component={Home} /> */}
+        {/* <Route exact path='/game' component={Game} /> */}
+        <Route exact path='/login' component={Login} />
         <Route exact path='/register' component={Register} />
       </Container>
     </Router>
